356928ea4e3bee58e9dbfc87314d3a09758735e9
ititicekiti <154707539+ititicekiti@users.noreply.github.com>
2023-12-31 19:46:24 +0300

    fff

---
+++ script.js
@@ -0,0 +1,17 @@
+const giris = document.querySelector("#deneme");
+const ekle = document.querySelector("#ekle");;
+const liste = document.querySelector("#liste")
+
+ekle.addEventListener("submit",tetikIti)
+
+tetikIti function(e)
+{
+    asagiEkle();
+    preventDefault();
+}
+
+asagiEkle function()
+{
+    
+
+}
